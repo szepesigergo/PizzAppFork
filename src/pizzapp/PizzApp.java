593639dcae71a681a05bbbfef07e1be605ccb8a0
Szepesi Gergő Manó  (SZF_2023_2025) <szepesi.gergo.mano@diak.szamalk-szalezi.hu>
2023-10-24 11:52:56 +0200

    combo actionperformed() szamol

--- src/pizzapp/PizzApp.java
+++ src/pizzapp/PizzApp.java
@@ -4,7 +4,8 @@ public class PizzApp extends javax.swing.JFrame {
 
     public PizzApp() {
         initComponents();
-        int alapAr2 = 1750;
+        
+        int alapAr = 1750;
         
         double meretSzorzo = 1;
         
@@ -15,7 +16,7 @@ public class PizzApp extends javax.swing.JFrame {
         int extra3 = 0;
         int extrak = extra1 + extra2 + extra3;
         
-        double vegsoAr = alapAr2 * meretSzorzo * db + extrak;
+        double vegsoAr = alapAr * meretSzorzo * db + extrak;
         vegsoAr *=db;
                 
         lblAr.setText(vegsoAr + "");
@@ -235,7 +236,35 @@ public class PizzApp extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void cmdValaszthatoPizzakActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmdValaszthatoPizzakActionPerformed
+        // melyik a választott pizza indexe:
+        int pizzaIndex = cmdValaszthatoPizzak.getSelectedIndex();
         
+        //választott pizza alapára:
+        int alapAr = -1; // -1 jelentése: nincs ára a pizzának 
+        if(pizzaIndex == 0){
+                alapAr = 1580;
+        }else if(pizzaIndex == 1){
+                alapAr = 1680;
+        }else if(pizzaIndex == 2){
+                alapAr = 1750;
+        }else if(pizzaIndex == 3){
+                alapAr = 2100;
+        }
+        
+        
+        double meretSzorzo = 1;
+        
+        int db = 1;
+        
+        int extra1 = 0;
+        int extra2 = 0;
+        int extra3 = 0;
+        int extrak = extra1 + extra2 + extra3;
+        
+        double vegsoAr = alapAr * meretSzorzo * db + extrak;
+        vegsoAr *=db;
+                
+        lblAr.setText(vegsoAr + "");
     }//GEN-LAST:event_cmdValaszthatoPizzakActionPerformed
 
     public static void main(String args[]) {
